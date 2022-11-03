144005f3803871236c61e5687fabb3ca0d8073e9
FatchurRozi <91874081+MuhammadFatchurRozi@users.noreply.github.com>
2022-11-03 18:51:36 +0700

    update 3-11-2022 15:51

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -7,8 +7,9 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span></button>
-            <a class="navbar-brand" href="{{ route('home') }}"><span>ERP</span> PRODUKSI KAOS POLOS</a>
-            <ul class="nav navbar-top-links navbar-right">
+            <a class="navbar-brand" href="{{ route('home') }}"><span>ERP</span> PRODUKSI KAOS
+                POLOS</a>
+            {{-- <ul class="nav navbar-top-links navbar-right">
                 <li class="dropdown"><a class="dropdown-toggle count-info" data-toggle="dropdown" href="#">
                         <em class="fa fa-envelope"></em><span class="label label-danger">15</span>
                     </a>
@@ -66,7 +67,7 @@
                             </a></li>
                     </ul>
                 </li>
-            </ul>
+            </ul> --}}
         </div>
     </div>
 </nav>
