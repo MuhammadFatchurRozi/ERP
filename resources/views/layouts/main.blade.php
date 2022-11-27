6970f8923e39f40b05ba8f8128bb54a75566bb3d
FatchurRozi <91874081+MuhammadFatchurRozi@users.noreply.github.com>
2022-11-27 13:59:22 +0700

    title image

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="{{ asset('style/gambar/title.png') }}" type="image/icon type">
     <title>ERP</title>
     <link href="{{ asset('style/css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('style/css/font-awesome.min.css') }}" rel="stylesheet">
